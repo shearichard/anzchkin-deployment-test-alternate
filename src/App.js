9a96354d69dbcc2c5a34632edca0409ca0305ba4
shearichard-nec <richard.shea@nec.co.nz>
2023-06-15 21:23:28 +1200

    Add notes to README

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from "react";
 import Webcam from "react-webcam";
 
 function App() {
+
+     /* 
+          */
   return (
     <div className="App">
       <h2>Here's a demonstration camera view</h2>
@@ -15,10 +18,10 @@ function App() {
       It's important to remember that to host the 'real' app some other things need to be in place, and these are...
       </p>
       <ul>
-          <li>Either the CORS problem with NDP has to be resolved, or somehow, the requests to NDP must be run though a proxy, as they are in dev.</li>
-          <li>User needs to be using an IP address which NDP has whitelisted</li>
-          <li>The Regula Forensics container needs to be running somewhere which is accessible from the browser running the demo</li>
-          <li>The Regula Forensics container needs its license updated.</li>
+          <li>Either the CORS problem with MEQ has to be resolved, or somehow, the requests to MEQ must be run though a proxy, as they are in dev.</li>
+          <li>User needs to be using an IP address which MEQ has whitelisted</li>
+          <li>The RF container needs to be running somewhere which is accessible from the browser running the demo</li>
+          <li>The RF container needs its license updated.</li>
       </ul>
     </div>
   );
