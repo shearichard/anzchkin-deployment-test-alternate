db5f2127e84fcaeba976d6218af097818ae4d818
shearichard-nec <richard.shea@nec.co.nz>
2023-06-15 20:08:48 +1200

    Make a rudimentary camera in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import Webcam from "react-webcam";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Here's a demonstration camera view</h2>
+      <Webcam />
     </div>
   );
 }
